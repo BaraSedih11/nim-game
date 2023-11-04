4a3afa577e0a72c860f5f2f34f92c6fce987e6fd
BaraSedih11 <98843912+BaraSedih11@users.noreply.github.com>
2023-11-04 02:00:20 +0200

    vvv

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(express.static('public'));
 
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port bbb ${port}...`);
+  console.log(`Server is running on port ${port}...`);
 });
