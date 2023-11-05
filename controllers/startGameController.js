feeb06c5a671360a7dac7ab6901b0cd0b56423a7
bara <s12010060@stu.najeh.edu>
2023-11-05 22:20:14 +0200

    second

--- controllers/startGameController.js
+++ controllers/startGameController.js
@@ -1,37 +1,46 @@
 const path = require('path');
 
-exports.startGame = (req, res) => {
-    // Parse user input for name and game type
-    const username = req.body.username;
-    const gameType = req.body.gameType;
-    const sticks = req.body.sticks;
-    const level = req.body.level;
-  
-    // Initialize the game with user and computer players
-    const game = initializeGame(gameType, username, sticks, level);
-  
-    // Send game data to the client
-    res.json({ game });
-  }
+// exports.startGame = (req, res) => {
+//     console.log('Button clicked on the server');
 
+//     const { username, gameType, level, sticks } = req.body;
+    
+//     // Perform any server-side logic here and send a response back to the client
+//     const response = { message: 'Button click event received on the server' };
+//     res.json(response);
+//   }
 
- exports.loginPage = (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'loginPage', 'index.html'));
-  }
+//  exports.loginPage = (req, res) => {
+//     //res.sendFile(path.join(__dirname, '..', 'public', 'loginPage', 'index.html'));
 
-  function initializeGame(gameType, username, sticks, level) {
-    if (gameType === "NIM1") {
+//     console.log('Button clicked on the server');
+//     // You can access request data from req.body to get user input
+//     const { username, gameType, level, sticks } = req.body;
+    
+//     // Perform any server-side logic here
 
-        const nimGame = {
-            gameType: "NIM1",
-            username,
-            currentPlayer: "user", 
-            sticks, 
-            level
-        };
-        return nimGame;
-    } else {
-        // Handle other game types or return an error
-        throw new Error("Unsupported game type");
-    }
-}
+//     // Send a response to the client
+//     const response = { message: 'Button click event received on the server' };
+//     res.json(response);
+//   }
+
+//   function initializeGame(gameType, username, sticks, level) {
+//     if (gameType === "NIM1") {
+
+//         const nimGame = {
+//             gameType: "NIM1",
+//             username,
+//             currentPlayer: "user", 
+//             sticks, 
+//             level
+//         };
+//         return nimGame;
+//     } else {
+//         // Handle other game types or return an error
+//         throw new Error("Unsupported game type");
+//     }
+//}
+
+// exports.homePage = (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'loginPage', 'index.html'));
+// }
--- routes/startGameRouter.js
+++ routes/startGameRouter.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const startGameController = require('../controllers/startGameController');
+const path = require('path');
+//const startGameController = require('../controllers/startGameController');
 
 const routes = express.Router();
 
-routes.get('/login', startGameController.loginPage);
-  
-routes.post('/startGame', startGameController.startGame);
+routes.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the login page when the user accesses the root URL
+routes.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'loginPage', 'index.html')); 
+});
+
+routes.post('/checkGameType', (req, res) => {
+    // You can perform any server-side logic here if needed
+    // No need to check 'gameType' and return it in the response
+    res.json({ message: 'Game type checked successfully' });
+});
 
 module.exports = routes;
